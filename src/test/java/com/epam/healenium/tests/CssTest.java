1e0f370320b4da75d0d8188d1ff92523b50c15b2
Alex-Reif <69298932+Alex-Reif@users.noreply.github.com>
2025-01-21 20:33:45 +0300

    Update src/test/java/com/epam/healenium/tests/CssTest.java

--- src/test/java/com/epam/healenium/tests/CssTest.java
+++ src/test/java/com/epam/healenium/tests/CssTest.java
@@ -64,9 +64,9 @@ public class CssTest extends BaseTest {
         FrameworkPage page = pages.get(TEST_ENV);
 
         page.openPage()
-                .findTestElement(LocatorType.CSS, "test_tag")
+                .findTestElement(LocatorType.XPATH, "//*[@id='change_element']")
                 .clickSubmitButton()
-                .findTestElement(LocatorType.CSS, "test_tag");
+                .findTestElement(LocatorType.XPATH, "//*[@id='change_element']");
     }
 
     @Test
