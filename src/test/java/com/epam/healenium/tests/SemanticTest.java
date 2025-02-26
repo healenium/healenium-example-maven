8c23f8e227aa62f8cf45bba67b830a19c204c640
Alex-Reif <69298932+Alex-Reif@users.noreply.github.com>
2025-02-26 15:48:00 +0300

    Update src/test/java/com/epam/healenium/tests/SemanticTest.java

--- src/test/java/com/epam/healenium/tests/SemanticTest.java
+++ src/test/java/com/epam/healenium/tests/SemanticTest.java
@@ -51,9 +51,9 @@ public class SemanticTest extends BaseTest {
         FrameworkPage page = pages.get(TEST_ENV);
 
         page.openPage()
-                .findTestElement(LocatorType.LINK_TEXT, "Change: LinkText, PartialLinkText")
+                .findTestElement(By.xpath, "//*[@id='change_links']")
                 .clickSubmitButton()
-                .findTestElement(LocatorType.LINK_TEXT, "Change: LinkText, PartialLinkText");
+                .findTestElement(By.xpath, "//*[@id='change_links']");
     }
 
     @Test
@@ -63,9 +63,9 @@ public class SemanticTest extends BaseTest {
         FrameworkPage page = pages.get(TEST_ENV);
 
         page.openPage()
-                .findTestElement(LocatorType.NAME, "change_name")
+                .findTestElement(By.xpath, "//*[@id='newName']")
                 .clickSubmitButton()
-                .findTestElement(LocatorType.NAME, "change_name");
+                .findTestElement(By.xpath, "//*[@id='newName']");
     }
 
     @Test
@@ -75,9 +75,9 @@ public class SemanticTest extends BaseTest {
         FrameworkPage page = pages.get(TEST_ENV);
 
         page.openPage()
-                .findTestElement(LocatorType.PARTIAL_LINK_TEXT, "PartialLinkText")
+                .findTestElement(By.xpath, "//*[@id='change_links']")
                 .clickSubmitButton()
-                .findTestElement(LocatorType.PARTIAL_LINK_TEXT, "PartialLinkText");
+                .findTestElement(By.xpath, "//*[@id='change_links']");
     }
 
     @Test
@@ -87,9 +87,9 @@ public class SemanticTest extends BaseTest {
         FrameworkPage page = pages.get(TEST_ENV);
 
         page.openPage()
-                .findTestElement(LocatorType.TAG_NAME, "test_tag")
+                .findTestElement(By.xpath, "//*[@id='change_element']")
                 .clickSubmitButton()
-                .findTestElement(LocatorType.TAG_NAME, "test_tag");
+                .findTestElement(By.xpath, "//*[@id='change_element']");
     }
 
     @Test
