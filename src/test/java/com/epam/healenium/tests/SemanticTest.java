9f0d254f04c6c72264b4944732511e8fa2082d7e
Alex-Reif <69298932+Alex-Reif@users.noreply.github.com>
2025-02-26 16:10:39 +0300

    Update src/test/java/com/epam/healenium/tests/SemanticTest.java

--- src/test/java/com/epam/healenium/tests/SemanticTest.java
+++ src/test/java/com/epam/healenium/tests/SemanticTest.java
@@ -75,9 +75,9 @@ public class SemanticTest extends BaseTest {
         FrameworkPage page = pages.get(TEST_ENV);
 
         page.openPage()
-                .findTestElement(LocatorType.PARTIAL_LINK_TEXT, "PartialLinkText")
+                .findTestElement(LocatorType.XPATH, "//*[@id='change_links']")
                 .clickSubmitButton()
-                .findTestElement(LocatorType.PARTIAL_LINK_TEXT, "PartialLinkText");
+                .findTestElement(LocatorType.XPATH, "//*[@id='change_links']");
     }
 
     @Test
