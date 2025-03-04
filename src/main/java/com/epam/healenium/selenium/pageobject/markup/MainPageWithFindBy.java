c91789f94920af32d5ec36d4079cc20d0e58ebdb
Alex-Reif <69298932+Alex-Reif@users.noreply.github.com>
2025-03-04 17:28:46 +0300

    Update src/main/java/com/epam/healenium/selenium/pageobject/markup/MainPageWithFindBy.java

--- src/main/java/com/epam/healenium/selenium/pageobject/markup/MainPageWithFindBy.java
+++ src/main/java/com/epam/healenium/selenium/pageobject/markup/MainPageWithFindBy.java
@@ -46,7 +46,7 @@ public class MainPageWithFindBy extends SeleniumBasePage {
 
     @FindBy (name =  "change_name")
     WebElement inputFieldChangeName;
-    @FindBy (linkText = "Change: LinkText, PartialLinkText")
+    @FindBy (partialLinkText = "Change: LinkText, PartialLinkText")
     WebElement inputFieldChangeLinkText;
 
     @FindBy(id = "form_checked1")
