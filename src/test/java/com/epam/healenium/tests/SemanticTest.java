f86c308713fcd715cc40b79617d421b41ee406c3
Alex-Reif <69298932+Alex-Reif@users.noreply.github.com>
2025-03-11 13:24:12 +0300

    Update src/test/java/com/epam/healenium/tests/SemanticTest.java

--- src/test/java/com/epam/healenium/tests/SemanticTest.java
+++ src/test/java/com/epam/healenium/tests/SemanticTest.java
@@ -60,9 +60,9 @@ public class SemanticTest extends BaseTest {
         FrameworkPage page = pages.get(TEST_ENV);
 
         page.openPage();
-        driver.findElement(By.name("change_name"));
+        driver.findElement(By.name("healed_change_name"));
         page.clickSubmitButton();
-        driver.findElement(By.name("change_name"));
+        driver.findElement(By.name("healed_change_name"));
     }
 
     @Test
