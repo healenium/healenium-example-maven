ce948d16c3823c13bdd22ffc30108d24b1198299
helen_yrofeeva <helen_yrofeeva@epam.com>
2026-03-02 15:12:01 +0300

    [EPMHLM-501] update test-env link

--- src/main/java/com/epam/healenium/constants/PageUrl.java
+++ src/main/java/com/epam/healenium/constants/PageUrl.java
@@ -1,7 +1,7 @@
 package com.epam.healenium.constants;
 
 public enum PageUrl {
-    TEST_ENV_URL("https://elenastepuro.github.io/test_env/index.html"),
+    TEST_ENV_URL("https://healenium.github.io/healenium-test-env/index.html"),
     CALLBACK_URL("https://mdn.github.io/web-components-examples/life-cycle-callbacks/"),
     MARKUP_URL("http://sha-test-app.herokuapp.com/");
 
